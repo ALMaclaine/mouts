4b0bbaba5db44efcd7f19338df5d2441a9e910af
Alessandro Maclaine <almaclaine@gmail.com>
2022-10-04 11:32:41 -0500

    add create-object

---
+++ packages/mouts/src/object/create-object.ts
@@ -0,0 +1,11 @@
+import { mixIn } from './mix-in';
+
+const createObject = <T, R>(parent: T, props?: R): T & R => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  function F() {}
+  F.prototype = parent;
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  return mixIn(new F(), props);
+};
+export { createObject };
