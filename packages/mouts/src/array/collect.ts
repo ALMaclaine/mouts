eb66c2a033a1228e5ff461cec89f36d5782608b5
Alessandro Maclaine <almaclaine@gmail.com>
2022-10-05 14:13:55 -0500

    add filter/every array

--- packages/mouts/src/array/collect.ts
+++ packages/mouts/src/array/collect.ts
@@ -1,10 +1,7 @@
 import { append } from './append';
 
 // collect is like map except callback can return array of T
-const collect = <T>(
-  arr: T[],
-  cb: (val: T, i?: number, arr?: T[]) => T | T[]
-) => {
+const collect = <T>(arr: T[], cb: (val: T, i: number, arr: T[]) => T | T[]) => {
   const res: T[] = [];
   for (const [key, val] of arr.entries()) {
     append(res, cb(val, key, arr));
--- packages/mouts/src/array/filter-array.ts
+++ packages/mouts/src/array/filter-array.ts
@@ -1,7 +1,7 @@
-const everyArray = <T>(
+const filterArray = <T>(
   arr: T[],
   cb: (val: T, i: number, inArr: T[]) => boolean,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   thisArg?: any
-) => arr.every(cb, thisArg);
-export { everyArray };
+) => arr.filter(cb, thisArg);
+export { filterArray };
