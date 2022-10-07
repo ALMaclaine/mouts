3f3be9ddf1e427f61b829a13c7bd9c5891231429
Alessandro Maclaine <almaclaine@gmail.com>
2022-10-07 11:27:50 -0500

    add loop

---
+++ packages/mouts/src/math/loop.ts
@@ -0,0 +1,6 @@
+// unlike clamp, loop brings values around the other end
+const loop = (val: number, min: number, max: number) => {
+  const or = val > max ? min : val;
+  return val < min ? max : or;
+};
+export { loop };
